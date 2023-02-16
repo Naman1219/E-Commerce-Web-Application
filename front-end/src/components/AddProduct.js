462640fcc17900ee18632ceaca52cb1ed40a2470
naman <guptanaman1995@gmail.com>
2023-02-16 12:25:31 +0530

    Added Simple Form Validation

--- front-end/src/components/AddProduct.js
+++ front-end/src/components/AddProduct.js
@@ -5,9 +5,14 @@ const AddProduct = () => {
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
+  const [error, setError] = useState(false);
 
   const addProduct = async () => {
-    console.warn(name, price, category, company);
+    if (!name || !price || !company || !category) {
+      setError(true);
+      return false;
+    }
+    // console.warn(name, price, category, company);
     const userId = JSON.parse(localStorage.getItem('user'))._id;
     let result = await fetch("http://localhost:5000/add-product", {
       method: 'post',
@@ -24,16 +29,18 @@ const AddProduct = () => {
     <div className='product'>
       <h1>Add Product</h1>
       <input type="text" placeholder='Enter Product Name' className='inputBox'
-        onChange={(e) => { setName(e.target.value) }} value={name}
-      />
+        onChange={(e) => { setName(e.target.value) }} value={name} />
+      {error && !name && <span className='invalid_input'>Enter valid name</span>}
       <input type="text" placeholder='Enter Product Price' className='inputBox'
         onChange={(e) => { setPrice(e.target.value) }} value={price} />
+      {error && !price && <span className='invalid_input'>Enter valid price</span>}
       <input type="text" placeholder='Enter Product Category' className='inputBox'
-        onChange={(e) => { setCategory(e.target.value) }} value={category}
-      />
+        onChange={(e) => { setCategory(e.target.value) }} value={category} />
+      {error && !category && <span className='invalid_input'>Enter valid category</span>}
       <input type="text" placeholder='Enter Product Company' className='inputBox'
-        onChange={(e) => { setCompany(e.target.value) }} value={company}
-      />
+        onChange={(e) => { setCompany(e.target.value) }} value={company} />
+      {error && !company && <span className='invalid_input'>Enter valid company</span>}
+
       <button onClick={addProduct} className='appButton'>Add Product</button>
     </div>
   );
