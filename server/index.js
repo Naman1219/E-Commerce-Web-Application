00ab8810888e8b13e258b05d5ae13347bbc261b0
Dev <devgoel2004@gmail.com>
2024-02-17 00:23:30 +0530

    Update Password strength in backend and frontend

--- server/index.js
+++ server/index.js
@@ -1,34 +1,47 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-require('./db/config');
-const cors = require('cors');
-const User = require('./db/User');
-const Product = require('./db/Product');
+require("./db/config");
+const cors = require("cors");
+const User = require("./db/User");
+const Product = require("./db/Product");
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+// route for register a user
 app.post("/register", async (req, res) => {
-  let user = new User(req.body);
-  let result = await user.save();
-  result = result.toObject();
-  delete result.password;
-  res.send(result);
+  const { name, email, password } = req.body;
+
+  const emailFound = await User.findOne({ email: email });
+  if (emailFound) {
+    console.log("User already register");
+    return res
+      .status(400)
+      .send({ success: false, message: "User already register" });
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  return res
+    .status(200)
+    .send({ success: true, message: "User Login Successfully", user });
 });
 
 app.post("/login", async (req, res) => {
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-      res.send(user);
-    }
-    else {
-      res.send({ result: 'No user found' });
+      return res
+        .status(200)
+        .send({ success: true, message: "User login Successfully", user });
+    } else {
+      return res.status(400).send({ success: false, message: "No user found" });
     }
   }
-  else {
-    res.send({ result: 'No user found' });
-  }
+  return res
+    .status(400)
+    .send({ success: false, message: "Invalid credentials" });
 });
 
 app.post("/add-product", async (req, res) => {
@@ -41,9 +54,8 @@ app.get("/products", async (req, res) => {
   let products = await Product.find();
   if (products.length > 0) {
     res.send(products);
-  }
-  else {
-    res.send({ result: "No products found" })
+  } else {
+    res.send({ result: "No products found" });
   }
 });
 
@@ -56,8 +68,7 @@ app.get("/product/:id", async (req, res) => {
   let result = await Product.findOne({ _id: req.params.id });
   if (result) {
     res.send(result);
-  }
-  else {
+  } else {
     res.send({ result: "No record found" });
   }
 });
@@ -65,24 +76,26 @@ app.get("/product/:id", async (req, res) => {
 app.put("/product/:id", async (req, res) => {
   let result = await Product.updateOne(
     {
-      _id: req.params.id
+      _id: req.params.id,
     },
     {
-      $set: req.body
+      $set: req.body,
     }
-  )
+  );
   res.send(result);
 });
 
 app.get("/search/:key", async (req, res) => {
   let result = await Product.find({
-    "$or": [
+    $or: [
       { name: { $regex: req.params.key } },
       { company: { $regex: req.params.key } },
-      { category: { $regex: req.params.key } }
-    ]
+      { category: { $regex: req.params.key } },
+    ],
   });
   res.send(result);
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server working");
+});
